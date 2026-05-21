19990515c8ba9e118723f46092288c35136c3973
Your Name <your.email@example.com>
2026-05-21 15:42:49 -0400

    Commit inicial - Projeto Calculadora Android

---
+++ app/src/main/java/com/example/calculadora/CalculatorLogic.kt
@@ -0,0 +1,31 @@
+package com.example.calculadora
+
+class CalculatorLogic {
+    //Adição
+    fun adicao(a: Double, b: Double): Double {
+        return a + b
+    }
+
+    //Subtração
+    fun subtracao(a: Double, b: Double): Double {
+        return a - b
+    }
+
+    //Multiplicação
+    fun multiplicacao(a: Double, b: Double): Double {
+        return a * b
+    }
+
+    //Divisão
+    fun divisao(a: Double, b: Double): Double {
+        if (b == 0.0) {
+            throw IllegalArgumentException("Divisão por zero não é permitida.")
+        }
+        return a / b
+    }
+
+    //Porcentagem
+    fun porcentagem(a: Double, b: Double): Double{
+        return (a + b) / 100
+    }
+}
