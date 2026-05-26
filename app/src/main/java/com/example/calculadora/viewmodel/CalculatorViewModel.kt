aae37e62dd31ed875aa57858207f5a035eaa42e0
Aira Aparecida <airaaparecida35@gmail.com>
2026-05-26 10:02:31 -0300

    Implementando a logica no CalculatorViewModel

--- app/src/main/java/com/example/calculadora/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/example/calculadora/viewmodel/CalculatorViewModel.kt
@@ -5,4 +5,30 @@ import androidx.compose.runtime.Composable
 @Composable
 fun CalculatorViewModel() {
     // Lógica de negócios e estado da calculadora
+    private val logic = CalculatorLogic()
+    var display by mutableState("0")
+    private set
+
+    private var numeroAnterior: Double? = null
+    private var operacao: String? = null
+
+    fun onkeyPressed(key: String){
+        when(key){
+            //"C" -> 
+            //"+"
+            //"-"
+            //"x"
+           // "/"
+
+           //"="->
+        }  
+    }
+
+    fun calcular(){
+        //val a 
+        //val b
+        //val op
+    }
+
+
 }
