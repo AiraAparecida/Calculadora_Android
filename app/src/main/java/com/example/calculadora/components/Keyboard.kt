c340304574d6c8c605556390d2f796d8a9ea46aa
Your Name <your.email@example.com>
2026-06-01 17:01:23 -0400

    implementando tratametos de erros

--- app/src/main/java/com/example/calculadora/components/Keyboard.kt
+++ app/src/main/java/com/example/calculadora/components/Keyboard.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,8 +16,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.calculadora.CalculatorParser
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -39,14 +41,16 @@ fun Keyboard() {
         verticalArrangement = Arrangement.Bottom
     ) {
 
-        OutlinedTextField(
+        BasicTextField(
             value = number,
             onValueChange = { number = it },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 18.dp, vertical = 8.dp)
-                .align(Alignment.End),
-            textStyle = MaterialTheme.typography.displayMedium,
+                .padding(horizontal = 18.dp, vertical = 8.dp),
+            textStyle = MaterialTheme.typography.displayMedium.copy(
+                fontSize = 60.sp,
+                textAlign = TextAlign.End
+            )
         )
 
         Column {
@@ -84,7 +88,7 @@ fun Keyboard() {
 }
 
 @Preview(
-    showBackground = true
+    showBackground = true,
 )
 @Composable
 fun KeyboardPreview() {
