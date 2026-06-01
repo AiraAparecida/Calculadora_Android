9bdd0c04599f74a9a753c525a7198e67b17e7e47
Your Name <your.email@example.com>
2026-06-01 14:22:42 -0400

    Implementacao da logica dentro de onclick

--- app/src/main/java/com/example/calculadora/components/Teclado.kt
+++ app/src/main/java/com/example/calculadora/components/Teclado.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,14 +16,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.calculadora.CalculatorLogic
 
 @OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun Teclado() {
+    var number by remember { mutableStateOf("") }
+
     val rows = listOf(
         listOf("⌫", "C", "%", "÷"),
         listOf("7", "8", "9", "x"),
@@ -36,19 +35,10 @@ fun Teclado() {
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Bottom
     ) {
-        var number by remember { mutableStateOf("10") }
-        if (number.isEmpty()) {
-            Text(
-                text = "Calculadora $number",
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth(),
-                style = MaterialTheme.typography.titleLarge
-            )
-        }
+
         OutlinedTextField(
             value = number,
-            onValueChange = { number = it},
+            onValueChange = { number = it },
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 18.dp, vertical = 8.dp)
@@ -68,11 +58,43 @@ fun Teclado() {
                         ButtonCalculadora(
                             label = key,
                             onClick = {
-                                //number = number + key
-                                if(key == "C"){
+                                if (key == "C") {
                                     number = ""
-                                }else if(key == "⌫"){
+                                } else if (key == "⌫") {
                                     number = number.dropLast(1)
+                                } else if (key == "=") {
+//                                    val result: Double
+//                                    if (number.contains("+")) {
+//                                        val parts = number.split("+")
+//                                        val a = parts[0].toDouble()
+//                                        val b = parts[1].toDouble()
+//                                        result = CalculatorLogic().adicao(a, b)
+//                                    } else if (key == "-") {
+//                                        val parts = number.split("-")
+//                                        val a = parts[0].toDouble()
+//                                        val b = parts[1].toDouble()
+//                                        result = CalculatorLogic().subtracao(a, b)
+//                                    } else if (key == "x") {
+//                                        val parts = number.split("x")
+//                                        val a = parts[0].toDouble()
+//                                        val b = parts[1].toDouble()
+//                                        result = CalculatorLogic().multiplicacao(a, b)
+//                                    } else if (key == "÷") {
+//                                        val parts = number.split("÷")
+//                                        val a = parts[0].toDouble()
+//                                        val b = parts[1].toDouble()
+//                                        result = CalculatorLogic().divisao(a, b)
+//                                    } else if (key == "%") {
+//                                        val parts = number.split("%")
+//                                        val a = parts[0].toDouble()
+//                                        val b = parts[1].toDouble()
+//                                        result = CalculatorLogic().porcentagem(a, b)
+//                                    }else{
+//                                        result = 0.0
+//                                    }
+//                                    number = result.toString()
+                                } else {
+                                    number = number + key
                                 }
                             })
                     }
