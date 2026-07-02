69263daa59586b22d21ab098eabe1594326b4394
Your Name <your.email@example.com>
2026-07-02 14:23:46 -0400

    Identificando mais de um Operador

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    id("io.github.takahirom.roborazzi") version "1.59.0" apply false
+    id("io.github.takahirom.roborazzi") version "1.8.0" apply false
 }
